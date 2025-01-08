4b1ad684dc6bc5a691739e30f19d6dc99d92644a
Aayush Chaudhary <aayushcyy@gmail.com>
2025-01-08 20:48:10 +0530

    homepage is responsive

--- app/Component/Card4Tablet.jsx
+++ app/Component/Card4Tablet.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useRef } from "react";
 import Candle from "./Candle";
 import Image from "next/image";
--- app/page.jsx
+++ app/page.jsx
@@ -21,6 +21,7 @@ export default function Home() {
   const [message, setMessage] = useState("");
   const [generatedLink, setGeneratedLink] = useState("");
   const [showWaitMsg, setShowWaitMsg] = useState(false);
+  const [loader, setLoader] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -29,7 +30,7 @@ export default function Home() {
       alert("Please fill in all fields.");
       return;
     }
-
+    setLoader(true);
     const response = await fetch("/api/submit", {
       method: "POST",
       headers: {
@@ -42,6 +43,7 @@ export default function Home() {
     if (result.id) {
       setGeneratedLink(`${window.location.origin}/user/${result.id}`);
       setDisplayCard(true);
+      setLoader(false);
     }
 
     console.log(name, age, message);
@@ -51,12 +53,12 @@ export default function Home() {
   };
 
   return (
-    <div className="flex lg:flex-row flex-col w-full min-h-screen items-center text-center">
+    <div className="flex flex-col w-full min-h-screen items-center text-center">
       <Image
         src={gift}
         width={180}
         height={180}
-        className="w-[7rem] absolute right-6 top-[21rem] rotate-12 z-0"
+        className="w-[6rem] xl:w-[9rem] absolute right-4 top-[22rem] xl:top-[80%]  rotate-12 z-0"
         alt=""
       />
       <Image
@@ -70,7 +72,7 @@ export default function Home() {
         src={balloon}
         width={180}
         height={180}
-        className="w-[7rem] absolute left-3 top-28 opacity-90 -rotate-12 z-0"
+        className="w-[6rem] xl:w-[10rem] absolute left-0 top-64 xl:top-28 opacity-90 -rotate-12 z-0"
         alt=""
       />
       <Image
@@ -81,22 +83,22 @@ export default function Home() {
         alt=""
       />
       {!displayCard && (
-        <div className="flex flex-col items-center gap-28 min-h-screen pb-0">
-          <div className="flex relative w-full pt-12 text-center flex-col items-center">
-            <div className="bg-[#E7B251] w-[65%] h-[5rem] absolute rotate-[8deg] top-[68px]  skewed-div2"></div>
+        <div className="flex flex-col w-full sm:flex-row items-center sm:justify-between xl:justify-center gap-28 xl:gap-32 sm:gap-0 min-h-screen pb-0">
+          <div className="flex relative w-full sm:w-[42%] sm:h-full pt-12 sm:pt-0 text-center flex-col items-center">
+            <div className="bg-[#E7B251] w-[65%] sm:w-[80%] xl:w-[82%] h-[5rem] sm:h-[6rem] xl:h-[11rem] absolute rotate-[8deg] top-[20%] sm:top-[4%]  skewed-div2"></div>
 
-            <div className="bg-[#E3645A] w-[65%] absolute top-[10rem] h-[6rem] skew-y-2 -skew-x-12 transform skewed-div"></div>
+            <div className="bg-[#E3645A] w-[67%] sm:w-[80%] xl:w-[90%] absolute top-[45%] sm:top-[35%] xl:top-[36%] h-[6rem] sm:h-[7rem] xl:h-[12rem] skew-y-2 -skew-x-12 transform skewed-div"></div>
 
-            <div className="bg-[#619FEB] w-[50%] absolute h-[6rem] top-[16rem] -rotate-[7deg] z-[5]"></div>
-            <p className="uppercase text-[5.1rem] text-[#020817] w-[90%] leading-tight font-barlow font-semibold z-[6] tracking-tight">
+            <div className="bg-[#619FEB] w-[50%] sm:w-[60%] absolute h-[5rem] sm:h-[6rem] xl:h-[11rem] top-[74%] sm:top-[69%] -rotate-[7deg] z-[5]"></div>
+            <p className="uppercase text-[23vw] sm:text-[12vw] text-[#020817] w-[90%] xl:leading-[1.1] leading-tight font-barlow font-semibold z-[6] tracking-tight">
               create a<br />
               birthday
               <br />
               card!
             </p>
           </div>
-          <div className="w-[85%] bg-white shadow-lg h-fit px-[1rem] pt-7 pb-4 flex flex-col gap-4 font-lexend mt-[rem] z-10 mb-10">
-            <p className="font-lexend font-normal text-sm">
+          <div className="w-[85%] sm:w-[40%] xl:w-[33%] bg-white shadow-lg h-fit px-[1rem] pt-7 md:pb-4 flex flex-col gap-4 font-lexend mt-[rem] z-10 mb-10 sm:mb-0 sm:mr-10">
+            <p className="font-lexend font-normal md:text-sm sm:text-xs text-xs">
               Enter the birthday person's name, age, and a custom message that
               will appear after they blow out their candles.
             </p>
@@ -105,14 +107,17 @@ export default function Home() {
               onSubmit={handleSubmit}
             >
               <div className="flex flex-col gap-1">
-                <label htmlFor="name" className="font-normal text-[1rem]">
+                <label
+                  htmlFor="name"
+                  className="font-normal md:text-lg text-[.9rem]"
+                >
                   Name
                 </label>
                 <div className="flex w-full border-2 px-2 py-2 gap-3 font-normal">
-                  <UserIcon className="size-6 stroke-2" />
+                  <UserIcon className="md:size-6 size-5 stroke-2" />
                   <input
                     type="text"
-                    className="outline-none"
+                    className="outline-none text-sm sm:text-xs md:text-lg"
                     id="name"
                     placeholder="Name"
                     value={name}
@@ -121,14 +126,17 @@ export default function Home() {
                 </div>
               </div>
               <div className="flex flex-col gap-1">
-                <label htmlFor="age" className="font-normal text-[1rem]">
+                <label
+                  htmlFor="age"
+                  className="font-normal md:text-lg text-[.9rem]"
+                >
                   Age
                 </label>
                 <div className="flex w-full border-2 px-2 py-2 gap-3 font-normal">
-                  <CalendarIcon className="size-6 stroke-2" />
+                  <CalendarIcon className="md:size-6 size-5 stroke-2" />
                   <input
                     type="number"
-                    className="outline-none w-full"
+                    className="outline-none w-full text-sm md:text-lg"
                     id="age"
                     placeholder="Age"
                     value={age}
@@ -137,14 +145,17 @@ export default function Home() {
                 </div>
               </div>
               <div className="flex flex-col gap-1">
-                <label htmlFor="msg" className="font-normal text-[1rem]">
+                <label
+                  htmlFor="msg"
+                  className="font-normal md:text-lg text-[.9rem]"
+                >
                   Message
                 </label>
                 <div className="flex w-full border-2 px-2 py-2 gap-3 font-normal">
-                  <ChatBubbleOvalLeftEllipsisIcon className="size-6 stroke-2" />
+                  <ChatBubbleOvalLeftEllipsisIcon className="md:size-6 size-5 stroke-2" />
                   <input
                     type="text"
-                    className="outline-none"
+                    className="outline-none text-sm md:text-lg"
                     id="msg"
                     placeholder="Message"
                     value={message}
@@ -154,7 +165,7 @@ export default function Home() {
               </div>
               <div className="h-[4.5rem] relative flex flex-col gap-0">
                 <button
-                  className="w-full bg-[#A855F7] text-white uppercase py-2 text-xl border-b-4 active:border-none border-[#643392] active:mt-1"
+                  className="w-full bg-[#A855F7] text-white uppercase md:py-2 py-[5px] text-lg border-b-4 active:border-none border-[#643392] active:mt-1"
                   type="submit"
                 >
                   Create
@@ -164,17 +175,19 @@ export default function Home() {
                     {/* <p className="text-xs text-red-600 bottom-0">
                       Please wait!
                     </p> */}
-                    <div className="relative">
-                      <DotLottieReact
-                        src="https://lottie.host/5a269627-0014-4d61-b508-06f1ea663f7d/yCKtYlLyee.lottie"
-                        loop={true}
-                        autoplay={true}
-                        style={{
-                          width: "140px",
-                          height: "70px",
-                        }}
-                      />
-                    </div>
+                    {loader && (
+                      <div className="relative">
+                        <DotLottieReact
+                          src="https://lottie.host/5a269627-0014-4d61-b508-06f1ea663f7d/yCKtYlLyee.lottie"
+                          loop={true}
+                          autoplay={true}
+                          style={{
+                            width: "120px",
+                            height: "60px",
+                          }}
+                        />
+                      </div>
+                    )}
                   </div>
                 )}
               </div>
@@ -183,7 +196,7 @@ export default function Home() {
         </div>
       )}
       {displayCard && <Envelope link={generatedLink} />}
-      <div className="h-10 w-full font-lexend text-xs ">
+      <div className="h-5 w-full font-lexend text-[10px] md:text-xs">
         Made with 💗 by Aayush
       </div>
     </div>
