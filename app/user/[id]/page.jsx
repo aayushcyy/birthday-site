5cc5d4b327a8d4ea24cd0a8343963836f8cf090d
Aayush Chaudhary <aayushcyy@gmail.com>
2025-01-09 15:04:59 +0530

    made changes to create your own link

--- app/user/[id]/page.jsx
+++ app/user/[id]/page.jsx
@@ -164,7 +164,7 @@ export default function UserPage({ params: paramsPromise }) {
         <p className="text-[10px]">
           Create you own{" "}
           <Link
-            href={"https://bday-card-chi.vercel.app/"}
+            href={"https://bdaycard-delta.vercel.app/"}
             className="text-purple-700 italic"
           >
             Click
